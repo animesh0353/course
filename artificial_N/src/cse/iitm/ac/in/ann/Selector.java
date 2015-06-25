60c8a0c3b40449f8e3a08554e710407dc885298c
anomesh0353 <animesh.0353@gmail.com>
2015-06-25 11:58:23 +0530

    Initial commit

---
+++ artificial_N/src/cse/iitm/ac/in/ann/Selector.java
@@ -0,0 +1,5 @@
+package cse.iitm.ac.in.ann;
+
+public interface Selector<K> {
+	public boolean test(K k);
+}
---
+++ artificial_N/src/cse/iitm/ac/in/ann/shashank.java
@@ -0,0 +1,10 @@
+package cse.iitm.ac.in.ann;
+
+public class shashank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
